e8293c941d9c846e5b6660484265c7b5ad713470
Daviduf11 <ufberg.david@gmail.com>
2021-01-24 01:37:35 -0500

    added handling of zero & negative numbers, end < start case; added full seed dataset

--- api/controllers/appApiControllers.js
+++ api/controllers/appApiControllers.js
@@ -1,19 +1,5 @@
-const e = require("express");
-
-const apps = [
-  { id: 3, name: "my-app-003" },
-  { id: 11, name: "my-app-011" },
-  { id: 6, name: "my-app-006" },
-  { id: 10, name: "my-app-010" },
-  { id: 7, name: "my-app-007" },
-  { id: 1, name: "my-app-001" },
-  { id: 2, name: "my-app-002" },
-  { id: 4, name: "my-app-004" },
-  { id: 12, name: "my-app-012" },
-  { id: 8, name: "my-app-008" },
-  { id: 5, name: "my-app-005" },
-  { id: 9, name: "my-app-009" },
-];
+const e = require("express"),
+  apps = require("../seedData");
 
 let sortById;
 let start;
@@ -30,7 +16,7 @@ const getApps = (req, res) => {
         apps.sort((a, b) => (a.id > b.id ? 1 : -1));
 
         start = req.query.start ? Number(req.query.start) : 1;
-        max = req.query.max ? Number(req.query.max) : 5;
+        max = req.query.max ? Number(req.query.max) : 50;
         end = req.query.end ? Number(req.query.end) : start + max - 1;
       } else if (req.query.by === "name") {
         sortById = false;
@@ -39,7 +25,7 @@ const getApps = (req, res) => {
         start = req.query.start
           ? Number(req.query.start.slice(req.query.start.length - 3))
           : 1;
-        max = req.query.max ? Number(req.query.max) : 5;
+        max = req.query.max ? Number(req.query.max) : 50;
         end = req.query.end
           ? Number(req.query.end.slice(req.query.end.length - 3))
           : start + max - 1;
@@ -47,13 +33,18 @@ const getApps = (req, res) => {
         throw 'Invalid "by" value. The only valid values are "id" and "name".';
     }
 
-    console.log("start: ", start, "max: ", max, "end: ", end);
-
     if (req.query.max) {
       if (end > start + max) {
         end = start + max - 1;
       }
     }
+
+    if (start < 1)
+      throw 'Invalid query. "Start" value must be greater than zero.';
+    if (end < 1) throw 'Invalid query. "End" value must be greater than zero.';
+    if (max < 1) throw 'Invalid query. "Max" value must be greater than zero.';
+    if (end < start)
+      throw 'Invalid query. "End" value cannot be less than "start" value.';
 
     if (req.query.order) {
       if (req.query.order === "desc") {
