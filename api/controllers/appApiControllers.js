31c0a3253c5ea862d01ad84a4fa36f1b842f9efd
Daviduf11 <ufberg.david@gmail.com>
2021-01-24 15:42:25 -0500

    fixed desc order bug; added res.send() for error messages

--- api/controllers/appApiControllers.js
+++ api/controllers/appApiControllers.js
@@ -5,11 +5,14 @@ let sortById;
 let start;
 let max;
 let end;
+let matchingApps;
 
 const getApps = (req, res) => {
   if (JSON.stringify(req.query) !== "{}") {
     if (!req.query.by) {
-      throw 'Invalid query. "By" paramater is required; valid values are "id" and "name".';
+      res.send(
+        'Invalid query. "By" paramater is required; valid values are "id" and "name".'
+      );
     } else {
       if (req.query.by === "id") {
         sortById = true;
@@ -30,7 +33,9 @@ const getApps = (req, res) => {
           ? Number(req.query.end.slice(req.query.end.length - 3))
           : start + max - 1;
       } else
-        throw 'Invalid "by" value. The only valid values are "id" and "name".';
+        res.send(
+          'Invalid "by" value. The only valid values are "id" and "name".'
+        );
     }
 
     if (req.query.max) {
@@ -44,7 +49,9 @@ const getApps = (req, res) => {
     if (end < 1) throw 'Invalid query. "End" value must be greater than zero.';
     if (max < 1) throw 'Invalid query. "Max" value must be greater than zero.';
     if (end < start)
-      throw 'Invalid query. "End" value cannot be less than "start" value.';
+      res.send('Invalid query. "End" value cannot be less than "start" value.');
+
+    matchingApps = apps.slice(start - 1, end);
 
     if (req.query.order) {
       if (req.query.order === "desc") {
@@ -52,7 +59,9 @@ const getApps = (req, res) => {
           ? matchingApps.sort((a, b) => (a.id < b.id ? 1 : -1))
           : matchingApps.sort((a, b) => (a.name < b.name ? 1 : -1));
       } else if (req.query.order !== "asc")
-        throw 'Invalid "order" value. The only valid values are "asc" and "desc".';
+        res.send(
+          'Invalid "order" value. The only valid values are "asc" and "desc".'
+        );
     }
   } else {
     apps.sort((a, b) => (a.id > b.id ? 1 : -1));
@@ -61,7 +70,6 @@ const getApps = (req, res) => {
     end = start + max - 1;
   }
 
-  const matchingApps = apps.slice(start - 1, end);
   res.json(matchingApps);
 };
 
