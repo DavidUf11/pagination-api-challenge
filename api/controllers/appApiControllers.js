a7f5b52c1a00163259704bdc5ec7d0222a73fccc
Daviduf11 <ufberg.david@gmail.com>
2021-01-24 00:19:03 -0500

    added check for range in query

--- api/controllers/appApiControllers.js
+++ api/controllers/appApiControllers.js
@@ -15,52 +15,62 @@ const apps = [
   { id: 9, name: "my-app-009" },
 ];
 
-let sortById = true;
+let sortById;
 let start;
 let max;
 let end;
 
 const getApps = (req, res) => {
-  if (req.query.by) {
-    if (req.query.by === "id") {
-      apps.sort((a, b) => (a.id > b.id ? 1 : -1));
+  if (JSON.stringify(req.query) !== "{}") {
+    if (!req.query.by) {
+      throw 'Invalid query. "By" paramater is required; valid values are "id" and "name".';
+    } else {
+      if (req.query.by === "id") {
+        sortById = true;
+        apps.sort((a, b) => (a.id > b.id ? 1 : -1));
 
-      start = req.query.start ? Number(req.query.start) : 1;
-      max = req.query.max ? Number(req.query.max) : 5;
-      end = req.query.end ? Number(req.query.end) : start + max - 1;
-    } else if (req.query.by === "name") {
-      apps.sort((a, b) => (a.name > b.name ? 1 : -1));
-      sortById = false;
+        start = req.query.start ? Number(req.query.start) : 1;
+        max = req.query.max ? Number(req.query.max) : 5;
+        end = req.query.end ? Number(req.query.end) : start + max - 1;
+      } else if (req.query.by === "name") {
+        sortById = false;
+        apps.sort((a, b) => (a.name > b.name ? 1 : -1));
 
-      start = req.query.start
-        ? Number(req.query.start.slice(req.query.start.length - 3))
-        : 1;
-      max = req.query.max ? Number(req.query.max) : 5;
-      end = req.query.end
-        ? Number(req.query.end.slice(req.query.end.length - 3))
-        : start + max - 1;
-    } else
-      throw 'Invalid "by" value. The only valid values are "id" and "name".';
-  } else
-    throw 'Invalid query. "By" paramater is required; valid values are "id" and "name".';
-
-  if (req.query.max) {
-    if (end > start + max) {
-      end = start + max - 1;
+        start = req.query.start
+          ? Number(req.query.start.slice(req.query.start.length - 3))
+          : 1;
+        max = req.query.max ? Number(req.query.max) : 5;
+        end = req.query.end
+          ? Number(req.query.end.slice(req.query.end.length - 3))
+          : start + max - 1;
+      } else
+        throw 'Invalid "by" value. The only valid values are "id" and "name".';
     }
+
+    console.log("start: ", start, "max: ", max, "end: ", end);
+
+    if (req.query.max) {
+      if (end > start + max) {
+        end = start + max - 1;
+      }
+    }
+
+    if (req.query.order) {
+      if (req.query.order === "desc") {
+        sortById
+          ? matchingApps.sort((a, b) => (a.id < b.id ? 1 : -1))
+          : matchingApps.sort((a, b) => (a.name < b.name ? 1 : -1));
+      } else if (req.query.order !== "asc")
+        throw 'Invalid "order" value. The only valid values are "asc" and "desc".';
+    }
+  } else {
+    apps.sort((a, b) => (a.id > b.id ? 1 : -1));
+    start = 1;
+    max = 5;
+    end = start + max - 1;
   }
 
   const matchingApps = apps.slice(start - 1, end);
-
-  if (req.query.order) {
-    if (req.query.order === "desc") {
-      sortById
-        ? matchingApps.sort((a, b) => (a.id < b.id ? 1 : -1))
-        : matchingApps.sort((a, b) => (a.name < b.name ? 1 : -1));
-    } else if (req.query.order !== "asc")
-      throw 'Invalid "order" value. The only valid values are "asc" and "desc".';
-  }
-
   res.json(matchingApps);
 };
 
